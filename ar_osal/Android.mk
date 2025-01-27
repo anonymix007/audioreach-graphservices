a333e5380db9edee28c3ffb9fb818792fb372bf0
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2025-01-27 22:04:52 +0300

    Convert header libs to Android.bp

--- ar_osal/Android.mk
+++ ar_osal/Android.mk
@@ -1,12 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libarosal_headers
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/api
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_HEADER_LIBRARY)
-
-include $(CLEAR_VARS)
 
 #----------------------------------------------------------------------------
 #                 Common definitons
